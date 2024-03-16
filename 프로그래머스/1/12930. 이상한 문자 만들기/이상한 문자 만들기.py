9ede5a9f2a53e738746575806d2d7afb7ce287b7
서영호 <dudgh9477@smail.kongju.ac.kr>
2024-03-16 23:06:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,11 +1,3 @@
 def solution(s):
     answer = ''
-    s_split = s.split(' ')
-    for i in s_split :
-        for j in range(len(i)):
-            if j % 2 == 0:
-                answer += i[j].upper()
-            else :
-                answer += i[j].lower()
-        answer += ' '       
-    return answer[:-1]
+    return answer
