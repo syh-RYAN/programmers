1858fc26c98e5cec96a374c29ae3f7bf0ebcc8f7
서영호 <dudgh9477@smail.kongju.ac.kr>
2024-04-22 11:09:21 +0900

    [level 2] Title: 주식가격, Time: 89.66 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,10 +1,3 @@
 def solution(prices):
-    answer = [0] * len(prices)
-    
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            answer[i] +=1
-            if prices[i] > prices[j]:
-                break
-    
+    answer = []
     return answer
